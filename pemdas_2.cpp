493d591b47a3cec52ddbb2d4acb4ad75aeed2bf4
al20250140008 <alf80169@gmail.com>
2026-02-23 11:59:36 +0700

    membuat programing

--- pemdas_2.cpp
+++ pemdas_2.cpp
@@ -1,8 +1,15 @@
-#include <iostream>
+#include <iostream> 
 using namespace std;
 using std::cout;
 using std::endl;
 
 int main(){
+
+    cout << "selamat datang di TI UMY" << endl;
+
+
+}cout << 10;
+
+
 
 }
